d6904ace28c5039a27096f59927f41625e1aa88e
Laci <doralaci98@gmail.com>
2020-04-02 17:07:17 +0200

    Persistence updated

--- Project3/Newmazon/Persistence/IPersistence.cs
+++ Project3/Newmazon/Persistence/IPersistence.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Newmazon.Persistence
 {
-    class IPersistence
+    public interface IPersistence
     {
+        Task<AllData> LoadAsync(String name);
+
+        Task<ICollection<Simulations>> ListAsync();
     }
 }
--- Project3/Newmazon/Persistence/NewmazonFileDataAccess.cs
+++ Project3/Newmazon/Persistence/NewmazonFileDataAccess.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Newmazon.Persistence
 {
-    class NewmazonFileDataAccess
+    public class NewmazonFileDataAccess
     {
-    }
+        private String _simDirectory;
+
+        public NewmazonFileDataAccess(String simDirectory)
+        {
+            _simDirectory = simDirectory;
+        }
+
+		/*
+		public async Task<AllData> LoadAsync(String name)
+		{
+
+		}
+		*/
+
+		public async Task<ICollection<Simulations>> ListAsync()
+		{
+			try
+			{
+				return Directory.GetFiles(_simDirectory, "*.stl")
+					.Select(path => new Simulations
+					{
+						Name = Path.GetFileNameWithoutExtension(path),
+					})
+					.ToList();
+			}
+			catch
+			{
+				throw new NewmazonDataExcception();
+			}
+		}
+	}
 }
