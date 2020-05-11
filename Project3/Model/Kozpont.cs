24607821bec649eea3954e32a2c0636894aeb55d
Jakab Olivér <jakaboliver98@gmail.com>
2020-05-11 21:22:45 +0200

    Több teszt esetet adtam még hozzá

--- Project3/Model/Kozpont.cs
+++ Project3/Model/Kozpont.cs
@@ -14,7 +14,7 @@ namespace Newmazon.Model
         //public List<List<NewmazonClasses>> table;
         public NewmazonClasses[,] table;
         public List<Robot> robots;
-        private List<List<Step>> paths;
+        public List<List<Step>> paths;
         private int startingEnergy;
         public List<Goods> goods;
         private int totalEnergyUsed;
